0a828af3aa326d9e28e1086c86d804fb4dd02975
MIguelluizsm <xxx987654331@gmail.com>
2026-01-06 18:35:02 -0300

    Update ConversorTemperatura.java

--- conversor-temperatura/ConversorTemperatura.java
+++ conversor-temperatura/ConversorTemperatura.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+
+public class ConversorTemperatura {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("=== Conversor de Temperatura ===");
+        System.out.print("Digite a temperatura em Celsius: ");
+        double celsius = input.nextDouble();
+      
+        double fahrenheit = (celsius * 9/5) + 32;
+        double kelvin = celsius + 273.15;
+
+        System.out.println("\nResultado:");
+        System.out.println("Celsius: " + celsius + " °C");
+        System.out.println("Fahrenheit: " + fahrenheit + " °F");
+        System.out.println("Kelvin: " + kelvin + " K");
+
+        input.close();
+    }
+}
 
